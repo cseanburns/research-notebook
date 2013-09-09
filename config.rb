04a1f6408592f918b36c469f9a8a1b93e1cd6610
C. Sean Burns <sean.csb@gmail.com>
2013-09-09 15:03:44 -0400

    Updated configs

--- config.rb
+++ config.rb
@@ -4,18 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-<<<<<<< HEAD
 http_path = "/journal"
 http_images_path = "/journal/images"
+http_generated_images_path = "/images"
 http_fonts_path = "/journal/fonts"
 css_dir = "/journal/public/stylesheets"
-=======
-http_path = "/"
-http_images_path = "/images"
-http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
->>>>>>> 3e1707d289b557d794bfaacbb6682a008223d4c9
 
 # Local development paths
 sass_dir = "sass"
